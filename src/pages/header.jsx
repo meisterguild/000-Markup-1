860fa01788aad011be52dd4011984db90c8ec9c8
SawadaMoemi <mo.sawada@m-gild.com>
2022-11-02 11:29:23 +0900

    ハンバーガーメニュー実装

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -1,11 +1,24 @@
 import styled from 'styled-components'
 import Logo from '../img/logo.svg'
+import HamburgerIcon from '../img/hamburgerIcon.svg'
+import { SideMenu } from 'pages/sideMenu'
+import { useState } from 'react'
 
 export const Header = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <>
       <SHeader>
-        <SImg src={Logo} alt='ロゴ' />
+        <SLogo src={Logo} alt='ロゴ' />
+        <SButton onClick={() => setOpen((open) => !open)}>
+          <SHamburgerIcon src={HamburgerIcon} alt='ロゴ' />
+        </SButton>
+        <SSideMenu>
+          <div className={` ${open ? 'isOpen' : 'isClose'}`}>
+            <SideMenu />
+          </div>
+        </SSideMenu>
       </SHeader>
     </>
   )
@@ -15,10 +28,41 @@ const SHeader = styled.header`
   height: 72px;
   width: 100%;
   background-color: #fff;
+  display: flex;
+  justify-content: space-between;
 `
-const SImg = styled.img`
+const SLogo = styled.img`
   width: 180px;
   height: 54px;
   margin-top: 10px;
   margin-left: 18px;
+`
+
+const SButton = styled.button`
+  display: flex;
+  align-items: center;
+  :hover {
+    cursor: pointer;
+  }
+`
+
+const SHamburgerIcon = styled.img`
+  width: 30px;
+  height: 20px;
+
+  margin-right: 18px;
+`
+
+const SSideMenu = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 50%;
+
+  .isClose {
+    display: none;
+  }
+  .isOpen {
+    display: block;
+  }
 `
--- src/pages/sideMenu.jsx
+++ src/pages/sideMenu.jsx
@@ -7,17 +7,35 @@ export const SideMenu = () => {
       <Wrapper>
         <SImg src={Logo} alt='ロゴ' />
         <SUl class='c-sidebar__siteLinkList'>
-          <li class='c-sidebar__siteLink'>私たちについて</li>
-          <li class='c-sidebar__siteLink'>サービス</li>
-          <li class='c-sidebar__siteLink'>商品情報</li>
-          <li class='c-sidebar__siteLink'>展示会</li>
-          <li class='c-sidebar__siteLink'>暮らしの日記</li>
-          <li class='c-sidebar__siteLink'>会社概要</li>
+          <li class='c-sidebar__siteLink'>
+            <a href='./###'>私たちについて</a>
+          </li>
+          <li class='c-sidebar__siteLink'>
+            <a href='./###'>サービス</a>
+          </li>
+          <li class='c-sidebar__siteLink'>
+            <a href='./###'>商品情報</a>
+          </li>
+          <li class='c-sidebar__siteLink'>
+            <a href='./###'>展示会</a>
+          </li>
+          <li class='c-sidebar__siteLink'>
+            <a href='./###'>暮らしの日記</a>
+          </li>
+          <li class='c-sidebar__siteLink'>
+            <a href='./###'>会社概要</a>
+          </li>
         </SUl>
         <SUl class='c-sidebar__snsLinkList'>
-          <li class='c-sidebar__snsLink'>Twitter</li>
-          <li class='c-sidebar__snsLink'>facebook</li>
-          <li class='c-sidebar__snsLink'>instagram</li>
+          <li class='c-sidebar__snsLink'>
+            <a href='./###'>Twitter</a>
+          </li>
+          <li class='c-sidebar__snsLink'>
+            <a href='./###'>facebook</a>
+          </li>
+          <li class='c-sidebar__snsLink'>
+            <a href='./###'>instagram</a>
+          </li>
         </SUl>
       </Wrapper>
     </SDiv>
@@ -36,6 +54,7 @@ const Wrapper = styled.div`
 `
 const SUl = styled.ul`
   &:not(:last-child) {
+    margin-top: 40px;
     margin-bottom: 60px;
   }
   .c-sidebar__siteLink {
